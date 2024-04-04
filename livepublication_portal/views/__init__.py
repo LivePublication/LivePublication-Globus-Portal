1eb1d76fff950bf57de1133c81b355cd701e03fb
Augustus Ellerm <ellermaugustus@gmail.com>
2024-04-05 10:10:27 +1300

    switching rel imports out

--- livepublication_portal/views/__init__.py
+++ livepublication_portal/views/__init__.py
@@ -1,7 +1,8 @@
-from .home_view import home
-from .flow_views.HelloWorld_flow_view import hello_flow
-from .flow_views.BeeMovie_flow_view import bee_flow
-from .flow_views.ls_flow_view import ls_flow
-from .publications_view import publications
-from .system_status_view import system_status
-from .control_centre_view import control_centre
+from livepublication_portal.views.home_view import home
+from livepublication_portal.views.flow_views.HelloWorld_flow_view import hello_flow
+from livepublication_portal.views.flow_views.BeeMovie_flow_view import bee_flow
+from livepublication_portal.views.flow_views.ls_flow_view import ls_flow
+from livepublication_portal.views.publications_view import publications
+from livepublication_portal.views.system_status_view import system_status
+from livepublication_portal.views.control_centre_view import control_centre
+

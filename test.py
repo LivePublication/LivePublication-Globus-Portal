d8257e08b3ed54821d05aec419d4dd879d26a329
Augustus Ellerm <ellermaugustus@gmail.com>
2024-04-16 14:07:56 +1200

    modified test function due to serrialisation issues between python versions (e.g. compute node version vs host script version)

--- test.py
+++ test.py
@@ -1,18 +1,23 @@
 from globus_compute_sdk import Executor
 
-from globus_compute_sdk.sdk.login_manager.whoami import print_whoami_info
+# from globus_compute_sdk.sdk.login_manager.whoami import print_whoami_info
 
-print_whoami_info()
+# print_whoami_info()
+
+from globus_compute_sdk import Client, Executor
+from globus_compute_sdk.serialize import CombinedCode
 
 # First, define the function ...
 def add_func(a, b):
     return a + b
 
-tutorial_endpoint_id = 'dc4ed5b5-3b27-4561-9f53-d561a4993744'
-# ... then create the executor, ...
-with Executor(endpoint_id=tutorial_endpoint_id) as gce:
+gcc = Client(code_serialization_strategy=CombinedCode())
+with Executor('58fb6f2d-ff78-4f39-9669-38c12d01f566', client=gcc) as gcx:
+    # do something with gcx
     # ... then submit for execution, ...
-    future = gce.submit(add_func, 5, 10)
+    future = gcx.submit(add_func, 5, 10)
 
     # ... and finally, wait for the result
     print(future.result())
+
+
